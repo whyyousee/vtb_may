3c43eac7e31977768dba13141b3b861c0a94dd6e
whyyousee <49287000+whyyousee@users.noreply.github.com>
2019-05-26 19:55:17 +0600

    попап работает (но есть скролл и кнопка при активации остается обычной)

--- js/main.js
+++ js/main.js
@@ -1,5 +1,28 @@
-$(document).ready(function () {
+let popup = document.getElementById('popupContact'),
+	popupToggle = document.getElementById('popupBtn'),
+	popupToggle2 = document.getElementById('popupMail'),
+	popupBtnStyle = document.getElementById('popupMail'),
+	popupClose = document.querySelector('.popupClose');
 
+	popupToggle.onclick = function() {
+		popup.style.display="block";
+	};
+
+	popupToggle2.onclick = function() {
+	popup.style.display="block";
+	};
+
+	popupClose.onclick = function () {
+		popup.style.display="none";
+	}
+
+	window.onclick = function (e) {
+		if(e.target == popup) {
+			popup.style.display="none";
+		}
+	}
+
+$(document).ready(function () {
 
 $('.gallery').slick({
   dots: false,
