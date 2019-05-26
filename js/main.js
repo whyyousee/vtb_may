78c48f4acee2122feb4b8bc8c8d16f3c644bce94
whyyousee <49287000+whyyousee@users.noreply.github.com>
2019-05-26 23:12:35 +0600

    попап v.1

--- js/main.js
+++ js/main.js
@@ -1,24 +1,41 @@
 let popup = document.getElementById('popupContact'),
 	popupToggle = document.getElementById('popupBtn'),
 	popupToggle2 = document.getElementById('popupMail'),
+	body = document.querySelector('body'),
 	popupBtnStyle = document.getElementById('popupMail'),
 	popupClose = document.querySelector('.popupClose');
 
 	popupToggle.onclick = function() {
 		popup.style.display="block";
+		body.style.overflow="hidden";
+		popupToggle.style.backgroundColor="#a01a43";
+		popupToggle.style.color="#fff";
+		popupToggle.style.textDecoration="none";
 	};
 
 	popupToggle2.onclick = function() {
-	popup.style.display="block";
+		popup.style.display="block";
+		body.style.overflow="hidden";
+		popupToggle.style.backgroundColor="#a01a43";
+		popupToggle.style.color="#fff";
+		popupToggle.style.textDecoration="none";
 	};
 
 	popupClose.onclick = function () {
 		popup.style.display="none";
+		body.style.overflow="auto";
+		popupToggle.style.backgroundColor="transparent";
+		popupToggle.style.color="#121435";
+		popupToggle.style.textDecoration="underline";
 	}
 
 	window.onclick = function (e) {
 		if(e.target == popup) {
 			popup.style.display="none";
+			body.style.overflow="auto";
+			popupToggle.style.backgroundColor="transparent";
+			popupToggle.style.color="#121435";
+			popupToggle.style.textDecoration="underline";
 		}
 	}
 
